42f151c63887d19db093fac0e77f4691cb65b04a
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2022-03-02 14:40:13 +0200

    Adds a unit testing project for the application

---
+++ test/ApplicationTests/Common/Utils/PointTests.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationTests;
+
+[TestClass]
+public class UnitTest1 {
+    [TestMethod]
+    public void TestMethod1() {
+
+    }
+}
+
---
+++ test/ApplicationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+
+[assembly: AssemblyTitle("ApplicationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApplicationTests")]
+[assembly: AssemblyCopyright("Copyright © Aleksi Lukkarinen 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("600531d4-d9a2-4d86-bb74-c0cddb79ebbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
