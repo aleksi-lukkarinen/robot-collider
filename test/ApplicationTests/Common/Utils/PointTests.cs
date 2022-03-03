6e5be2d00471effc90ecec616c418550279fb413
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2022-03-04 00:17:16 +0200

    Refactors and creates tests for Point

--- test/ApplicationTests/Common/Utils/PointTests.cs
+++ test/ApplicationTests/Common/Utils/PointTests.cs
@@ -6,14 +6,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
 public class PointTests {
+    private const int testX = 1;
+    private const int testY = 2;
+
+    private readonly Point testPoint = new(testX, testY);
+
     [TestMethod]
-    public void Created_Point_Has_Correct_Coordinates() {
-        const int testX = 1;
-        const int testY = 2;
-
-        Point p = new(testX, testY);
-
-        Assert.AreEqual(testX, p.X);
-        Assert.AreEqual(testY, p.Y);
+    public void Point_Has_Correct_Coordinates() {
+        Assert.AreEqual(testX, testPoint.X);
+        Assert.AreEqual(testY, testPoint.Y);
     }
+
+    [TestMethod]
+    public void Point_Has_Correct_String_Representation() {
+        Assert.AreEqual("(1, 2)", testPoint.ToString());
+    }
+
+    [TestMethod]
+    public void Equality_Is_Determined_Correctly() {
+        Assert.AreEqual(testPoint, testPoint);
+        Assert.AreEqual(new Point(testX, testY), testPoint);
+        Assert.AreNotEqual(new Point(testX + 1, testY), testPoint);
+        Assert.AreNotEqual(new Point(testX, testY + 1), testPoint);
+        Assert.AreNotEqual(new Point(testX + 1, testY + 1), testPoint);
+    }
+
+    [TestMethod]
+    public void Point_Has_Correct_Adjacent_Coordinates() {
+        Assert.AreEqual(new Point(testX - 1, testY), testPoint.Next(Direction.Left));
+        Assert.AreEqual(new Point(testX + 1, testY), testPoint.Next(Direction.Right));
+        Assert.AreEqual(new Point(testX, testY - 1), testPoint.Next(Direction.Up));
+        Assert.AreEqual(new Point(testX, testY + 1), testPoint.Next(Direction.Down));
+    }
+
 }
