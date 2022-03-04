caa7ffe3498802115ec6d2b9bd6a8d4eb425e599
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2022-03-04 02:11:03 +0200

    Adds doc comments to Direction

--- src/Application/Common/Utils/Direction.cs
+++ src/Application/Common/Utils/Direction.cs
@@ -5,9 +5,20 @@
 namespace ColliderApp.Common.Utils;
 
 
+/// <summary>
+/// Enum <c>Direction</c> models the possibility to chooce
+/// one of four directions: up, right, down, and left.
+/// </summary>
 public enum Direction {
+    /// <summary>Represents upwards direction.</summary>
     Up = 0,
+
+    /// <summary>Represents rightwards direction.</summary>
     Right = 1,
+
+    /// <summary>Represents downwards direction.</summary>
     Down = 2,
+
+    /// <summary>Represents leftwards direction.</summary>
     Left = 3,
 }
