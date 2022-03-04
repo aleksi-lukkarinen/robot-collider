eeb5ae2f97a574815212dce36ffa2861e03e973c
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2022-03-04 12:32:59 +0200

    Adds comments to ITurningStrategy.

--- src/Application/Common/Collider/ITurningStrategy.cs
+++ src/Application/Common/Collider/ITurningStrategy.cs
@@ -7,6 +7,14 @@ namespace ColliderApp.Common.Collider;
 using ColliderApp.Common.Utils;
 
 
+/// <summary>
+/// An interface for strategies for turning based on current direction.
+/// </summary>
 internal interface ITurningStrategy {
+    /// <summary>
+    /// Computes a new direction based on the given current direction.
+    /// </summary>
+    /// <param name="currentDirection">The current direction.</param>
+    /// <returns>A new direction.</returns>
     Direction NextDirection(Direction currentDirection);
 }
